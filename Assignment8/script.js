985efe03a55fb7bc2b1491fb1089439bc08f962d
Moazzam Ahmed <43891179+moazzamgodil@users.noreply.github.com>
2020-07-12 01:48:47 +0500

    Update

--- Assignment8/script.js
+++ Assignment8/script.js
@@ -1,17 +1,45 @@
-var min = document.getElementById("mins");
-var sec = document.getElementById("sec");
-var msec = document.getElementById("msec");
-var mintime = sectime = msectime = 0;
+var hourdisp = document.getElementById("hours");
+var mindisp = document.getElementById("mins");
+var secdisp = document.getElementById("sec");
+var hour = 1;
+var min = 1;
+var sec = 0;
 var interval;
 
 function timer() {
-    msectime++;
-    msec.innerHTML = msectime;
-    if(msec > 100) {
-        sectime++;
-        sec.innerHTML = sectime;
-        msec = 0;
+    sec++;
+    secdisp.innerHTML = sec;
+    if (sec > 59) {
+        mindisp.innerHTML = min;
+        min++;
+        sec = 0;
     }
+    else if (min > 59) {
+        hourdisp.innerHTML = hour;
+        hour++;
+        min = 0;
+    }
+
 }
 
-interval = setInterval(timer, 10);
+function start() {
+    interval = setInterval(timer, 1000);
+    document.getElementById("start").style.display = "none";
+    document.getElementById("pause").style.display = "inline-block";
+}
+
+function pause() {
+    clearInterval(interval);
+    document.getElementById("start").style.display = "inline-block";
+    document.getElementById("pause").style.display = "none";
+}
+
+function reset() {
+    clearInterval(interval);
+    hour = min = sec = 0;
+    hourdisp.innerHTML = hour;
+    mindisp.innerHTML = min;
+    secdisp.innerHTML = sec;
+    document.getElementById("start").style.display = "inline-block";
+    document.getElementById("pause").style.display = "none";
+}
